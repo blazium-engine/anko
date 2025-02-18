730f859c32ea9eb6c0b00e3d3018fd5f1191ccde
Dragos Daian <daian.dragos@yahoo.com>
2025-02-18 12:54:42 +0100

    Update walk_test.go

--- ast/astutil/walk_test.go
+++ ast/astutil/walk_test.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/mattn/anko/ast"
-	"github.com/mattn/anko/parser"
+	"github.com/blazium-engine/anko/ast"
+	"github.com/blazium-engine/anko/parser"
 )
 
 const (
